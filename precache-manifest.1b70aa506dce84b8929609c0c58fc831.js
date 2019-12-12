e8db5a4499f3ae63af3181120ad141cabf2e666d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-12 23:17:35 +0000

    Deploy DRFR0ST/vito-trans.pl to github.com/DRFR0ST/vito-trans.pl.git:gh-pages

---
+++ precache-manifest.1b70aa506dce84b8929609c0c58fc831.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd0f8d83f94db7a0701c5bf2b9dfec19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b4dfb1b12c261aa7445",
+    "url": "/static/js/2.c1b4ca42.chunk.js"
+  },
+  {
+    "revision": "a0ef13af3efa465e963c182b994fe51c",
+    "url": "/static/js/2.c1b4ca42.chunk.js.LICENSE"
+  },
+  {
+    "revision": "64a0de8f1903ebf91dbf",
+    "url": "/static/js/main.e9e3e6b5.chunk.js"
+  },
+  {
+    "revision": "c98faa04c3c46a0c1515",
+    "url": "/static/js/runtime-main.2a6e4f4e.js"
+  },
+  {
+    "revision": "83c9cb08603260aec2e281f5fd76e870",
+    "url": "/static/media/logo.83c9cb08.png"
+  },
+  {
+    "revision": "f679d9e67c0386a103cb600366ef3ecc",
+    "url": "/static/media/logo_white.f679d9e6.png"
+  },
+  {
+    "revision": "81561050b663c90c67fc1c809ba58859",
+    "url": "/static/media/thumbnail.81561050.jpeg"
+  }
+]);
